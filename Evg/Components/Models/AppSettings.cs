4b0e5ef675d289a0bf5873554eb75f0b67a35667
Evgeny <evgeny.fedotov93@gmail.com>
2024-03-11 10:09:08 +0200

    DataBase ControllerAdd project files.

---
+++ Evg/Components/Models/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Evg.Components.Models;
+
+    public class AppSettings
+    {
+        public string ConnectionString {  get; set; }
+    }
+
---
+++ Evg/Controllers/DataBaseController.cs
@@ -0,0 +1,26 @@
+﻿using Evg.Components.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Evg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DataBaseController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DataBaseController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("connectionString")]
+        public IActionResult GetConnectionString()
+        {
+            string dbConnectionString = _configuration.GetConnectionString("DbCnStr");
+
+            return Ok(new { ConnectionStrings = dbConnectionString });
+        }
+    }
+}
