ab476916f59ab985a92daa5417a0ea10237e5863
Evgeny <evgeny.fedotov93@gmail.com>
2024-03-20 13:27:45 +0200

    Переписываю метод Edit разными способами
    Добавил тестовую Dto Person2Dto

--- Evg/Controllers/PersonController.cs
+++ Evg/Controllers/PersonController.cs
@@ -1,6 +1,7 @@
 ﻿using Evg.Components.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace Evg.Controllers
 {
@@ -42,7 +43,9 @@ namespace Evg.Controllers
                 person.FirstName.Any(c => !char.IsLetter(c)) ||
                 person.LastName.Any(c => !char.IsLetter(c)) ||
                 person.FirstName.Contains(" ") ||
-                person.LastName.Contains(" "))
+                person.LastName.Contains(" ") ||
+                person.FirstName.Length < 2 ||
+                person.LastName.Length < 2)
             {
                 ModelState.AddModelError("", "First name and last name must start with a capital letter and should contain only letters.");
             }
@@ -57,6 +60,124 @@ namespace Evg.Controllers
 
             return Ok("Person updated successfully: " + person.FirstName + " " + person.LastName);
         }
+
+
+
+        [HttpPost]
+        public IActionResult Edit3(Person2Dto person)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState
+                    .Where(x => x.Value.Errors.Any())
+                    .Select(x => new { Property = x.Key, Errors = x.Value.Errors.Select(e => e.ErrorMessage) })
+                    .ToList();
+
+                return BadRequest(errors);
+            }
+
+            person.FirstName = "Andrew";
+            person.LastName = "Mayers";
+
+            return Ok("Person updated successfully: " + person.FirstName + " " + person.LastName);
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Edit2(Person2Dto person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return ValidationProblem();
+            }
+
+            person.FirstName = "Andrew";
+            person.LastName = "Mayers";
+
+            return Ok("Person updated successfully: " + person.FirstName + " " + person.LastName);
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Edit1(PersonDto person)
+        {
+            if (!char.IsUpper(person.FirstName[0]))
+            {
+                ModelState.AddModelError("", "First name must start with a capital letter.");
+            }
+
+            if (!char.IsUpper(person.LastName[0]))
+            {
+                ModelState.AddModelError("", "Last name must start with a capital letter.");
+            }
+
+            if (Regex.IsMatch(person.FirstName, @"[A-Z]{2,}"))
+            {
+                ModelState.AddModelError("", "First name should not contain two or more capital letters.");
+            }
+
+            if (Regex.IsMatch(person.LastName, @"[A-Z]{2,}"))
+            {
+                ModelState.AddModelError("", "Last name should not contain two or more capital letters.");
+            }
+
+            if (person.FirstName.Any(char.IsDigit))
+            {
+                ModelState.AddModelError("", "First name should not contain numbers.");
+            }
+
+            if (person.LastName.Any(char.IsDigit))
+            {
+                ModelState.AddModelError("", "Last name should not contain numbers.");
+            }
+
+            if (person.FirstName.Any(c => !char.IsLetter(c)))
+            {
+                ModelState.AddModelError("", "First name should contain only letters..");
+            }
+
+            if (person.LastName.Any(c => !char.IsLetter(c)))
+            {
+                ModelState.AddModelError("", "Last name should contain only letters..");
+            }
+
+            if (person.FirstName.Contains(" "))
+            {
+                ModelState.AddModelError("", "First name should not contain spaces.");
+            }
+
+            if (person.LastName.Contains(" "))
+            {
+                ModelState.AddModelError("", "Last name should not contain spaces.");
+            }
+
+            if (person.FirstName.Length < 2)
+            {
+                ModelState.AddModelError("", "First name should contain 2 or more letters.");
+            }
+
+            if (person.LastName.Length < 2)
+            {
+                ModelState.AddModelError("", "Last name should contain 2 or more letters..");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return ValidationProblem();
+            }
+
+            person.FirstName = "Andrew";
+            person.LastName = "Mayers";
+
+            return Ok("Person updated successfully: " + person.FirstName + " " + person.LastName);
+        }
+
+
+
 
 
         [HttpPost]
